b6a3a3735d5536d86e444faa49205c13eeebd7bf
Alluz Energia <alluzenergia@gmail.com>
2022-06-25 04:03:22 -0300

    create loader template and js

---
+++ resources/js/loader.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $(window).on('load', function () {
+        setTimeout(function () {
+            $('#loader').hide();
+        }, 500)
+
+    });
+
+})
---
+++ resources/views/loader.blade.php
@@ -0,0 +1,21 @@
+<div id="loader">
+    <img src="/img/loader.gif" width="70" style="z-index: 10;background-color: #fff; border-radius: 100%">
+    <span style="color: #004d88; font-size: 16pt; margin-top: 20px">Carregando, aguarde ...</span>
+</div>
+
+<style>
+    #loader {
+        width: 100%;
+        height: 100%;
+        background: rgb(241, 242, 243);
+        z-index: 10;
+        position: fixed;
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        transition: width 1s ease-out;
+    }
+</style>
